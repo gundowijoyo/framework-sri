e33f4b515f562f371d34a3ac79f6309c89b9aa56
wijoyo ✔ <133162689+gundowijoyo@users.noreply.github.com>
2024-09-11 12:43:30 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,9 +2,6 @@
 (function() {
     var scripts = [
         'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js'
-      //  'https://code.jquery.com/jquery-3.5.1.slim.min.js',
-     //   'https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js',
-      //  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'
     ];
 
     scripts.forEach(function(src) {
